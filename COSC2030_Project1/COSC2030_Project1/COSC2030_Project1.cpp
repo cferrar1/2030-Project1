d85630e14f5daa2b86a185e2cc614343314dd602
cferrar1 <43153834+cferrar1@users.noreply.github.com>
2018-10-26 12:22:43 -0600

    Add files via upload

--- COSC2030_Project1/COSC2030_Project1/COSC2030_Project1.cpp
+++ COSC2030_Project1/COSC2030_Project1/COSC2030_Project1.cpp
@@ -3,25 +3,27 @@
 #include "Day.h"
 #include "Week.h"
 #include "LinkedList.h"
+#include <string>
+#include <sstream>
 
+bool isFloat(string input);
 
 int main()
 {
-	float input;
+	string input;
 	int day = 0;
 	week week1 = week();
 	week week2 = week();
 	while (day < 14)
 	{
-		cout << "Input a Measurement, N (100) for the Next Day, D (101) for a Daily Summary, or W (102) for a Weekly Summary: ";
+		cout << "Day " << day + 1 << ": Input a Measurement, N for the Next Day, D for a Daily Summary, or W for a Weekly Summary: ";
 		cin >> input;
 
-		if (input == 100)
+		if (input == "N" || input == "Next")
 		{
 			day++;
-			cout << "Advanced Day, now on Day " << day + 1 << endl;
 		}
-		else if (input == 101)
+		else if (input == "D" || input == "Day")
 		{
 			if (day < 7)
 			{
@@ -32,7 +34,7 @@ int main()
 				week2.days[day - 7].printDay();
 			}
 		}
-		else if (input == 102)
+		else if (input == "W" || input == "Week")
 		{
 			if (day < 7)
 			{
@@ -43,15 +45,16 @@ int main()
 				week2.printWeek();
 			}
 		}
-		else if (input > 0)
+		else if (isFloat(input))
 		{
+			float indata = stof(input);
 			if (day < 7)
 			{
-				week1.days[day].addData(input);
+				week1.days[day].addData(indata);
 			}
 			else
 			{
-				week2.days[day - 7].addData(input);
+				week2.days[day - 7].addData(indata);
 			}
 		}
 	}
@@ -61,4 +64,12 @@ int main()
 	week2.printWeek();
 
 	return 0;
+}
+
+bool isFloat(const string input) // From https://stackoverflow.com/questions/447206/c-isfloat-function
+{
+	istringstream iss(input);
+	float f;
+	iss >> noskipws >> f;
+	return (iss.eof() && !iss.fail());
 }
